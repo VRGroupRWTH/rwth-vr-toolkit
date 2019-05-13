e92e3ccd6ffec6f5ab762ee96845a684c4d84e07
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2019-05-13 09:55:13 +0200

    add logging to game mode

--- Source/DisplayClusterExtensions/Private/VirtualRealityGameMode.cpp
+++ Source/DisplayClusterExtensions/Private/VirtualRealityGameMode.cpp
@@ -3,6 +3,8 @@
 
 AVirtualRealityGameMode::AVirtualRealityGameMode() : Super()
 {
+  UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityGameMode Constructor started"));
 	if (!bIsDisplayClusterActive) return;
 	DefaultPawnClass = AVirtualRealityPawn::StaticClass();
+  UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityGameMode Constructor finished"));
 }
