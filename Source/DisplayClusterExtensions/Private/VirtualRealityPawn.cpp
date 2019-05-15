1ac24975cd1de15fe20aa79978d44db0d0517535
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2019-05-15 17:16:55 +0200

    add back in some code from VR Pawn

--- Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
+++ Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
@@ -14,6 +14,28 @@
 AVirtualRealityPawn::AVirtualRealityPawn(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn constructor started"));
+  AutoPossessPlayer = EAutoReceiveInput::Player0; // Necessary for receiving motion controller events.
+
+  Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
+  Movement->UpdatedComponent = RootComponent;
+
+  RotatingMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingMovement"));
+  RotatingMovement->UpdatedComponent = RootComponent;
+  RotatingMovement->bRotationInLocalSpace = false;
+  RotatingMovement->PivotTranslation = FVector::ZeroVector;
+  RotatingMovement->RotationRate = FRotator::ZeroRotator;
+
+  LeftMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftMotionController"));
+  LeftMotionController->SetupAttachment(RootComponent);
+  LeftMotionController->SetTrackingSource(EControllerHand::Left);
+  LeftMotionController->SetShowDeviceModel(true);
+  LeftMotionController->SetVisibility(false);
+
+  RightMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightMotionController"));
+  RightMotionController->SetupAttachment(RootComponent);
+  RightMotionController->SetTrackingSource(EControllerHand::Right);
+  RightMotionController->SetShowDeviceModel(true);
+  RightMotionController->SetVisibility(false);
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn constructor finished"));
 }
 
@@ -50,6 +72,54 @@ void                    AVirtualRealityPawn::BeginPlay                  ()
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn BeginPlay started"));
   Super::BeginPlay();
+  bUseControllerRotationYaw = true;
+  bUseControllerRotationPitch = true;
+  bUseControllerRotationRoll = true;
+
+  // Display cluster settings apply to all setups (PC, HMD, CAVE/ROLV) despite the unfortunate name due to being an UE4 internal.
+  TArray<AActor*> SettingsActors;
+  UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADisplayClusterSettings::StaticClass(), SettingsActors);
+  if (SettingsActors.Num() > 0)
+  {
+    ADisplayClusterSettings* Settings = Cast<ADisplayClusterSettings>(SettingsActors[0]);
+    Movement->MaxSpeed = Settings->MovementMaxSpeed;
+    Movement->Acceleration = Settings->MovementAcceleration;
+    Movement->Deceleration = Settings->MovementDeceleration;
+    Movement->TurningBoost = Settings->MovementTurningBoost;
+    BaseTurnRate = Settings->RotationSpeed;
+  }
+
+  if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster)
+  {
+    UInputSettings::GetInputSettings()->RemoveAxisMapping(FInputAxisKeyMapping(FName(TEXT("TurnRate")), EKeys::MouseX));
+    UInputSettings::GetInputSettings()->RemoveAxisMapping(FInputAxisKeyMapping(FName(TEXT("LookUpRate")), EKeys::MouseY));
+
+    // Requires a scene node called flystick in the config.
+    Flystick = IDisplayCluster::Get().GetGameMgr()->GetNodeById(TEXT("flystick"));
+
+    Forward = Flystick;
+    LeftHand = Flystick;
+    RightHand = Flystick;
+  }
+  else if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayConnected())
+  {
+    UInputSettings::GetInputSettings()->RemoveAxisMapping(FInputAxisKeyMapping(FName(TEXT("TurnRate")), EKeys::MouseX));
+    UInputSettings::GetInputSettings()->RemoveAxisMapping(FInputAxisKeyMapping(FName(TEXT("LookUpRate")), EKeys::MouseY));
+
+    LeftMotionController->SetVisibility(true);
+    RightMotionController->SetVisibility(true);
+
+    Forward = LeftMotionController;
+    LeftHand = LeftMotionController;
+    RightHand = RightMotionController;
+  }
+  else
+  {
+    Forward = RootComponent;
+    LeftHand = RootComponent;
+    RightHand = RootComponent;
+  }
+
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn BeginPlay finished"));
 }
 void                    AVirtualRealityPawn::Tick                       (float DeltaSeconds)
