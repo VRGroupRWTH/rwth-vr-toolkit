d76984b1cfede6eaa92d37686f4b31ea02049587
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2019-05-17 10:35:03 +0200

    test out on...Implementations, add other code again (commented out)

--- Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
+++ Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
@@ -42,21 +42,48 @@ AVirtualRealityPawn::AVirtualRealityPawn(const FObjectInitializer& ObjectInitial
 void                    AVirtualRealityPawn::OnForward_Implementation   (float Value)
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnForward_Implementation started"));
+  if (NavigationMode == EVRNavigationModes::NAV_MODE_FLY || IDisplayCluster::Get().GetClusterMgr()->IsStandalone())
+  {
+    AddMovementInput(Forward->GetForwardVector(), Value);
+  }
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnForward_Implementation finished"));
 }
 void                    AVirtualRealityPawn::OnRight_Implementation(float Value)
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnRight_Implementation started"));
+  if (NavigationMode == EVRNavigationModes::NAV_MODE_FLY || IDisplayCluster::Get().GetClusterMgr()->IsStandalone())
+  {
+    AddMovementInput(Forward->GetRightVector(), Value);
+  }
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnRight_Implementation finished"));
 }
 void                    AVirtualRealityPawn::OnTurnRate_Implementation  (float Rate )
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnTurnRate_Implementation started"));
+  if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster)
+  {
+    const FVector CameraLocation = IDisplayCluster::Get().GetGameMgr()->GetActiveCamera()->GetComponentLocation();
+    RotatingMovement->PivotTranslation = RotatingMovement->UpdatedComponent->GetComponentTransform().InverseTransformPositionNoScale(CameraLocation);
+    RotatingMovement->RotationRate = FRotator(RotatingMovement->RotationRate.Pitch, Rate * BaseTurnRate, 0.0f);
+  }
+  else
+  {
+    AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+  }
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnTurnRate_Implementation finished"));
 }
 void                    AVirtualRealityPawn::OnLookUpRate_Implementation(float Rate )
 { 
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnLookUpRate_Implementation started"));
+  if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster)
+  {
+    // User-centered projection causes simulation sickness on look up interaction hence not implemented.
+  }
+  else
+  {
+    AddControllerPitchInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+  }
+
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn OnLookUpRate_Implementation finished"));
 }
 void                    AVirtualRealityPawn::OnFire_Implementation      (bool Pressed)
@@ -126,6 +153,17 @@ void                    AVirtualRealityPawn::Tick                       (float D
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn Tick started"));
   Super::Tick(DeltaSeconds);
+  // Flystick might not be available at start, hence is checked every frame.
+  //if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster && !Flystick)
+  //{
+  //  // Requires a scene node called flystick in the config.
+  //  Flystick = IDisplayCluster::Get().GetGameMgr()->GetNodeById(TEXT("flystick"));
+  //
+  //  Forward = Flystick;
+  //  LeftHand = Flystick;
+  //  RightHand = Flystick;
+  //}
+
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn Tick finished"));
 }
 void                    AVirtualRealityPawn::BeginDestroy               ()
@@ -138,7 +176,30 @@ void                    AVirtualRealityPawn::BeginDestroy               ()
 void                    AVirtualRealityPawn::SetupPlayerInputComponent  (UInputComponent* PlayerInputComponent)
 {
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn SetupPlayerInputComponent started"));
+  //check(PlayerInputComponent);
   Super::SetupPlayerInputComponent(PlayerInputComponent);
+  //if (PlayerInputComponent)
+  //{
+  //  PlayerInputComponent->BindAxis("MoveForward", this, &AVirtualRealityPawn::OnForward);
+  //  PlayerInputComponent->BindAxis("MoveRight", this, &AVirtualRealityPawn::OnRight);
+  //  PlayerInputComponent->BindAxis("TurnRate", this, &AVirtualRealityPawn::OnTurnRate);
+  //  PlayerInputComponent->BindAxis("LookUpRate", this, &AVirtualRealityPawn::OnLookUpRate);
+  //
+  //  PlayerInputComponent->BindAction<FFireDelegate>("Fire", IE_Pressed, this, &AVirtualRealityPawn::OnFire, true);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action1", IE_Pressed, this, &AVirtualRealityPawn::OnAction, true, 1);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action2", IE_Pressed, this, &AVirtualRealityPawn::OnAction, true, 2);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action3", IE_Pressed, this, &AVirtualRealityPawn::OnAction, true, 3);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action4", IE_Pressed, this, &AVirtualRealityPawn::OnAction, true, 4);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action5", IE_Pressed, this, &AVirtualRealityPawn::OnAction, true, 5);
+  //
+  //  PlayerInputComponent->BindAction<FFireDelegate>("Fire", IE_Released, this, &AVirtualRealityPawn::OnFire, false);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action1", IE_Released, this, &AVirtualRealityPawn::OnAction, false, 1);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action2", IE_Released, this, &AVirtualRealityPawn::OnAction, false, 2);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action3", IE_Released, this, &AVirtualRealityPawn::OnAction, false, 3);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action4", IE_Released, this, &AVirtualRealityPawn::OnAction, false, 4);
+  //  PlayerInputComponent->BindAction<FActionDelegate>("Action5", IE_Released, this, &AVirtualRealityPawn::OnAction, false, 5);
+  //}
+
   UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityPawn SetupPlayerInputComponent finished"));
 }
 UPawnMovementComponent* AVirtualRealityPawn::GetMovementComponent       () const
