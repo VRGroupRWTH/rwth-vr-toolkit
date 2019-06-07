6b9c5a3018e0f622972a11c457fe369033376b7d
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2019-06-07 10:02:05 +0200

    bin moveforward axis again

--- Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
+++ Source/DisplayClusterExtensions/Private/VirtualRealityPawn.cpp
@@ -154,12 +154,10 @@ void                    AVirtualRealityPawn::SetupPlayerInputComponent  (UInputC
   {
 
     // needs potentially [input_setup] id=dtrack_axis ch=0 bind="nDisplay Analog 0"
-    //PlayerInputComponent->BindAxis("MoveForward", this, &AVirtualRealityPawn::OnForward);
+    PlayerInputComponent->BindAxis("MoveForward", this, &AVirtualRealityPawn::OnForward);
     //PlayerInputComponent->BindAxis("MoveRight", this, &AVirtualRealityPawn::OnRight);
     //PlayerInputComponent->BindAxis("TurnRate", this, &AVirtualRealityPawn::OnTurnRate);
     //PlayerInputComponent->BindAxis("LookUpRate", this, &AVirtualRealityPawn::OnLookUpRate);
-
-    //PlayerInputComponent->BindAction<FAxisDelegate>("MoveForward", IE_Axis, this, &AVirtualRealityPawn::OnForward);
   }
 }
 UPawnMovementComponent* AVirtualRealityPawn::GetMovementComponent       () const
