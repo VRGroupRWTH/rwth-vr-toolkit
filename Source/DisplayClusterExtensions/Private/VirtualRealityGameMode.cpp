3e75a680c4a1f759f6b367a7746e8e8efd0395a0
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2019-06-11 10:08:45 +0200

    revert debug logging

--- Source/DisplayClusterExtensions/Private/VirtualRealityGameMode.cpp
+++ Source/DisplayClusterExtensions/Private/VirtualRealityGameMode.cpp
@@ -3,8 +3,6 @@
 
 AVirtualRealityGameMode::AVirtualRealityGameMode() : Super()
 {
-  UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityGameMode Constructor started"));
 	if (!bIsDisplayClusterActive) return;
 	DefaultPawnClass = AVirtualRealityPawn::StaticClass();
-  UE_LOG(LogTemp, Warning, TEXT("AVirtualRealityGameMode Constructor finished"));
 }
