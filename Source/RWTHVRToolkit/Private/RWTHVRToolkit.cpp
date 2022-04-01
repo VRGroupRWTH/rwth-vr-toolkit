894b60c3644cba7afc7aff4e169d2c09304af1a8
dg128746 <dg128746@ngm01.hpc.itc.rwth-aachen.de>
2022-04-01 13:59:04 +0200

    Removed livelink dtrack hack

--- Source/RWTHVRToolkit/Private/RWTHVRToolkit.cpp
+++ Source/RWTHVRToolkit/Private/RWTHVRToolkit.cpp
@@ -1,58 +1,15 @@
 #include "RWTHVRToolkit.h"
 
-#include "ILiveLinkClient.h"
-#include "LiveLinkClient.h"
-#include "RWTHVRToolkitSettings.h"
-
 #define LOCTEXT_NAMESPACE "FRWTHVRToolkitModule"
 
 void FRWTHVRToolkitModule::StartupModule ()
 {
 	ConsoleActivation.Register();
-
-	FCoreDelegates::OnFEngineLoopInitComplete.AddRaw(this, &FRWTHVRToolkitModule::OnEngineLoopInitComplete);
-
-#if WITH_EDITOR
-	GetMutableDefault<URWTHVRToolkitSettings>()->OnSettingChanged().AddRaw(this, &FRWTHVRToolkitModule::HandleSettingsSaved);
-#endif
 }
 
 void FRWTHVRToolkitModule::ShutdownModule()
 {
 	ConsoleActivation.Unregister();	
-}
-
-void FRWTHVRToolkitModule::OnEngineLoopInitComplete()
-{
-	ApplyDefaultPreset();
-}
-
-void FRWTHVRToolkitModule::HandleSettingsSaved(UObject* Settings, FPropertyChangedEvent& PropertyChangedEvent)
-{
-	ApplyDefaultPreset();
-}
-
-void FRWTHVRToolkitModule::ApplyDefaultPreset()
-{
-	ULiveLinkPreset* StartupPreset = GetDefault<URWTHVRToolkitSettings>()->DefaultLiveLinkPreset.LoadSynchronous();
-	if (StartupPreset != nullptr)
-	{
-		SetLiveLinkPreset(StartupPreset);
-	}
-}
-
-void FRWTHVRToolkitModule::SetLiveLinkPreset(ULiveLinkPreset* Preset)
-{
-	// We should check for a currently applied preset/sources now, and if it includes the ART one.
-	// If it does, and is active, do not apply it again. 
-
-	const FLiveLinkClient& LiveLinkClient = IModularFeatures::Get().GetModularFeature<FLiveLinkClient>(ILiveLinkClient::ModularFeatureName);
-	
-	if (LiveLinkPreset == nullptr && LiveLinkClient.GetSources().Num() == 0 )
-	{
-		LiveLinkPreset = Preset;
-		LiveLinkPreset->ApplyToClient();
-	}
 }
 
 
--- Source/RWTHVRToolkit/Public/RWTHVRToolkit.h
+++ Source/RWTHVRToolkit/Public/RWTHVRToolkit.h
@@ -3,7 +3,6 @@
 #include "CoreMinimal.h"
 #include "Fixes/ActivateConsoleInShipping.h"
 
-class ULiveLinkPreset;
 
 class FRWTHVRToolkitModule : public IModuleInterface
 {
@@ -11,14 +10,6 @@ public:
 	virtual void StartupModule () override;
 	virtual void ShutdownModule() override;
 	
-	void SetLiveLinkPreset(ULiveLinkPreset* Preset);
-
 private:
-
-	void OnEngineLoopInitComplete();
-	void HandleSettingsSaved(UObject* Settings, FPropertyChangedEvent& PropertyChangedEvent);
-	void ApplyDefaultPreset();
-	
 	FActivateConsoleInShipping ConsoleActivation;	
-	ULiveLinkPreset* LiveLinkPreset = nullptr;
 };
