56c0f205101e816069a47d9e49b8332009f3437e
David Gilbert <gilbert@vr.rwth-aachen.de>
2023-10-13 14:26:05 +0200

    Dynamically check if DTrackPlugin is actually there.

--- Source/RWTHVRCluster/RWTHVRCluster.Build.cs
+++ Source/RWTHVRCluster/RWTHVRCluster.Build.cs
@@ -17,27 +17,52 @@ public class RWTHVRCluster : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"RWTHVRToolkit",
-			"DisplayCluster",
-			"DeveloperSettings",
-            "InputCore",
-            "UMG",
-            "Slate",
-            "SlateCore",
-			"DTrackPlugin",
-			"DTrackInput"
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"DeveloperSettings",
+				"InputCore",
+				"UMG",
+				"Slate",
+				"SlateCore",
+				"RWTHVRToolkit"
 			}
 		);
+
+		if (IsPluginEnabledForTarget("nDisplay", base.Target))
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"DisplayCluster"
+				}
+			);
+		}
+		
+		if (IsPluginEnabledForTarget("DTrackPlugin", base.Target))
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"DTrackPlugin",
+					"DTrackInput"
+				}
+			);
+		}
 
 		PrivateDependencyModuleNames.AddRange(
 			new string[]{}
 		);
 
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[] { }
-        );
-    }
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[] { }
+		);
+	}
+
+	private static bool IsPluginEnabledForTarget(string PluginName, ReadOnlyTargetRules Target)
+	{
+		var PL = Plugins.GetPlugin(PluginName);
+		return PL != null && Target.ProjectFile != null && Plugins.IsPluginEnabledForTarget(PL,
+			ProjectDescriptor.FromFile(Target.ProjectFile), Target.Platform, Target.Configuration, Target.Type);
+	}
 }
