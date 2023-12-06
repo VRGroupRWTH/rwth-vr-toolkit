0e414ef8efafac42db28266afa9d41142df0ac32
jehret <ehret@vr.rwth-aachen.de>
2023-12-06 14:16:59 +0100

    style(clangformat) and minor formatting fic to the class description

--- Source/RWTHVRToolkit/Public/Pawn/Navigation/CollisionHandlingMovement.h
+++ Source/RWTHVRToolkit/Public/Pawn/Navigation/CollisionHandlingMovement.h
@@ -8,14 +8,14 @@
 
 /*
  * This Movement component is needed since in VR not only the pawn itself (UpdatedComponent) is moved but also the
- * user herself can walk and thereby move the CameraComponent, which can also lead to collisions or e.g. going up steps 
+ * user herself can walk and thereby move the CameraComponent, which can also lead to collisions or e.g. going up steps
  *
  * The four modes are:
- * None: No controller movement is applied and no corrections regarding steps or collisions with walls are done
- * Ghost: The same as above but now the Inputs can be used for unconstrained flying (also through objects)
- * Fly: The user can fly but not through walls etc. When the user walks against a wall the scene is moved with her to avoid walking through
- *      The user can also walk up stairs with a maximum step height of MaxStepHeight
- * Walk: Additionally to Fly now gravity keeps the user on the floor
+ * - None: No controller movement is applied and no corrections regarding steps or collisions with walls are done
+ * - Ghost: The same as above but now the Inputs can be used for unconstrained flying (also through objects)
+ * - Fly: The user can fly but not through walls etc. When the user walks against a wall the scene is moved with her to
+ *     avoid walking through The user can also walk up stairs with a maximum step height of MaxStepHeight
+ * - Walk: Additionally to Fly, now gravity keeps the user on the floor
  */
 
 UENUM(BlueprintType)
@@ -38,14 +38,14 @@ public:
 
 	void MoveOutOfNewDynamicCollisions();
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+							   FActorComponentTickFunction* ThisTickFunction) override;
 
 	void SetHeadComponent(USceneComponent* NewHeadComponent);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement")
 	EVRNavigationModes NavigationMode = EVRNavigationModes::NAV_WALK;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin="0.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin = "0.0"))
 	float MaxStepHeight = 40.0f;
 
 	// if the height that the pawn would fall (in walking mode) is higher
@@ -53,17 +53,17 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement")
 	float MaxFallingDepth = 1000.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMax="0.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMax = "0.0"))
 	float GravityAcceleration = -981.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin="0.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin = "0.0"))
 	float UpSteppingAcceleration = 981.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin="0.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR Movement", meta = (ClampMin = "0.0"))
 	float CapsuleRadius = 40.0f;
 
 private:
-	//check for
+	// check for
 	FHitResult CreateCapsuleTrace(const FVector& Start, const FVector& End, bool DrawDebug = false) const;
 	TOptional<FVector> GetOverlapResolveDirection() const;
 	void SetCapsuleColliderToUserSize() const;
@@ -82,7 +82,7 @@ private:
 	TOptional<FVector> LastCollisionFreeCapsulePosition;
 	FVector LastSteeringCollisionVector;
 
-	//just stored for performance gains;
+	// just stored for performance gains;
 	UPROPERTY(VisibleAnywhere, Transient, DuplicateTransient)
 	TArray<AActor*> ActorsToIgnore;
 };
