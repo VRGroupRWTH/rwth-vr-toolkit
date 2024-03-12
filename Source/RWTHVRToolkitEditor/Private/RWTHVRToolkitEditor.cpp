9b15677d3ce90c0bc5c4392ca8c61cef38121835
Timon Roemer <t.roemer@vis.rwth-aachen.de>
2024-03-12 11:03:13 +0100

    Cleans up and refactors

--- Source/RWTHVRToolkitEditor/Private/RWTHVRToolkitEditor.cpp
+++ Source/RWTHVRToolkitEditor/Private/RWTHVRToolkitEditor.cpp
@@ -1,5 +1,4 @@
 #include "RWTHVRToolkitEditor.h"
-
 #include "UnrealEdGlobals.h"
 #include "Editor/UnrealEdEngine.h"
 #include "Interaction/IntenSelectableCylinderScoringVisualizer.h"
@@ -11,79 +10,89 @@
 #include "Interaction/Interactables/IntenSelect/IntenSelectableRectangleScoring.h"
 #include "Interaction/Interactables/IntenSelect/IntenSelectableSphereScoring.h"
 
-IMPLEMENT_GAME_MODULE(FRWTHVRToolkitEditorModule, RWTHVRToolkitEditor);
-
-#include "RWTHVRToolkitEditor.h"
-#include "ComponentVisualizers.h"
-#include "Interaction/IntenSelectableLineScoringVisualizer.h"
-#include "UnrealEdGlobals.h"
-#include "Editor/UnrealEdEngine.h"
 #include "Interaction/IntenSelectableCircleScoringVisualizer.h"
 #include "Interaction/IntenSelectableCubeScoringVisualizer.h"
+#include "Interaction/IntenSelectableLineScoringVisualizer.h"
 #include "Interaction/IntenSelectableMultiPointScoringVisualizer.h"
 #include "Interaction/IntenSelectableRectangleScoringVisualizer.h"
 #include "Interaction/IntenSelectableSphereScoringVisualizer.h"
+
+IMPLEMENT_GAME_MODULE(FRWTHVRToolkitEditorModule, RWTHVRToolkitEditor);
 
 #define LOCTEXT_NAMESPACE "RWTHVRToolkitEditor"
 
 void FRWTHVRToolkitEditorModule::StartupModule()
 {
-	if (GUnrealEd != NULL)
+	if (GUnrealEd != nullptr)
 	{
-		const TSharedPtr<FComponentVisualizer> IntenSelectableLineVisualizer = MakeShareable(new FIntenSelectableLineScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableLineVisualizer =
+			MakeShareable(new FIntenSelectableLineScoringVisualizer());
+
 		if (IntenSelectableLineVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableLineScoring::StaticClass()->GetFName(), IntenSelectableLineVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableLineScoring::StaticClass()->GetFName(),
+												   IntenSelectableLineVisualizer);
 			IntenSelectableLineVisualizer->OnRegister();
 		}
 
-		
-		const TSharedPtr<FComponentVisualizer> IntenSelectableMultipointScoringVisualizer = MakeShareable(new FIntenSelectableMultiPointScoringVisualizer());
-		
+
+		const TSharedPtr<FComponentVisualizer> IntenSelectableMultipointScoringVisualizer =
+			MakeShareable(new FIntenSelectableMultiPointScoringVisualizer());
+
 		if (IntenSelectableMultipointScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableMultiPointScoring::StaticClass()->GetFName(), IntenSelectableMultipointScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableMultiPointScoring::StaticClass()->GetFName(),
+												   IntenSelectableMultipointScoringVisualizer);
 			IntenSelectableMultipointScoringVisualizer->OnRegister();
 		}
 
-		const TSharedPtr<FComponentVisualizer> IntenSelectableCircleScoringVisualizer = MakeShareable(new FIntenSelectableCircleScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableCircleScoringVisualizer =
+			MakeShareable(new FIntenSelectableCircleScoringVisualizer());
+
 		if (IntenSelectableCircleScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCircleScoring::StaticClass()->GetFName(), IntenSelectableCircleScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCircleScoring::StaticClass()->GetFName(),
+												   IntenSelectableCircleScoringVisualizer);
 			IntenSelectableCircleScoringVisualizer->OnRegister();
 		}
 
-		const TSharedPtr<FComponentVisualizer> IntenSelectableRectangleScoringVisualizer = MakeShareable(new FIntenSelectableRectangleScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableRectangleScoringVisualizer =
+			MakeShareable(new FIntenSelectableRectangleScoringVisualizer());
+
 		if (IntenSelectableRectangleScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableRectangleScoring::StaticClass()->GetFName(), IntenSelectableRectangleScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableRectangleScoring::StaticClass()->GetFName(),
+												   IntenSelectableRectangleScoringVisualizer);
 			IntenSelectableRectangleScoringVisualizer->OnRegister();
 		}
 
-		const TSharedPtr<FComponentVisualizer> IntenSelectableSphereScoringVisualizer = MakeShareable(new FIntenSelectableSphereScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableSphereScoringVisualizer =
+			MakeShareable(new FIntenSelectableSphereScoringVisualizer());
+
 		if (IntenSelectableSphereScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableSphereScoring::StaticClass()->GetFName(), IntenSelectableSphereScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableSphereScoring::StaticClass()->GetFName(),
+												   IntenSelectableSphereScoringVisualizer);
 			IntenSelectableSphereScoringVisualizer->OnRegister();
 		}
 
-		const TSharedPtr<FComponentVisualizer> IntenSelectableCubeScoringVisualizer = MakeShareable(new FIntenSelectableCubeScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableCubeScoringVisualizer =
+			MakeShareable(new FIntenSelectableCubeScoringVisualizer());
+
 		if (IntenSelectableCubeScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCubeScoring::StaticClass()->GetFName(), IntenSelectableCubeScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCubeScoring::StaticClass()->GetFName(),
+												   IntenSelectableCubeScoringVisualizer);
 			IntenSelectableCubeScoringVisualizer->OnRegister();
 		}
 
-		const TSharedPtr<FComponentVisualizer> IntenSelectableCylinderScoringVisualizer = MakeShareable(new FIntenSelectableCylinderScoringVisualizer());
-		
+		const TSharedPtr<FComponentVisualizer> IntenSelectableCylinderScoringVisualizer =
+			MakeShareable(new FIntenSelectableCylinderScoringVisualizer());
+
 		if (IntenSelectableCylinderScoringVisualizer.IsValid())
 		{
-			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCylinderScoring::StaticClass()->GetFName(), IntenSelectableCylinderScoringVisualizer);
+			GUnrealEd->RegisterComponentVisualizer(UIntenSelectableCylinderScoring::StaticClass()->GetFName(),
+												   IntenSelectableCylinderScoringVisualizer);
 			IntenSelectableCylinderScoringVisualizer->OnRegister();
 		}
 	}
@@ -91,7 +100,7 @@ void FRWTHVRToolkitEditorModule::StartupModule()
 
 void FRWTHVRToolkitEditorModule::ShutdownModule()
 {
-	if (GUnrealEd != NULL)
+	if (GUnrealEd != nullptr)
 	{
 		GUnrealEd->UnregisterComponentVisualizer(UIntenSelectableLineScoring::StaticClass()->GetFName());
 		GUnrealEd->UnregisterComponentVisualizer(UIntenSelectableMultiPointScoring::StaticClass()->GetFName());
