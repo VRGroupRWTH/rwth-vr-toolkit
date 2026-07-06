a8ab5332e9c6ba938bbbe04dd5c25df638690177
Andrew Puika <puika@vr.rwth-aachen.de>
2026-07-06 16:26:44 +0200

    just a minor spacing and breaklining cleanup

--- Source/RWTHVRToolkit/Private/Pawn/ClusterRepresentationActor.cpp
+++ Source/RWTHVRToolkit/Private/Pawn/ClusterRepresentationActor.cpp
@@ -1,8 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Pawn/ClusterRepresentationActor.h"
-
+﻿#include "Pawn/ClusterRepresentationActor.h"
 #include "Core/RWTHVRPlayerState.h"
 #include "Kismet/GameplayStatics.h"
 #include "Logging/StructuredLog.h"
